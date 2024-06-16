d4ee7942b80dce35f1d0434431798385de00ecd2
dahhnym <danimkim.dev@gmail.com>
2024-06-16 23:08:17 +0900

    chore: add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Tina Kim Front-end Developer`,
     description: `Tina's portfolio website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `http://dev-tina.netlify.app`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -15,6 +15,25 @@ const config: GatsbyConfig = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-postcss",
     "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Tina Kim Portfolio",
+        short_name: "Tina's Portfolio",
+        description: "This is to showcase a resume/portfolio of Tina Kim",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#fff",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 };
 
